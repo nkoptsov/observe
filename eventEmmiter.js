6ffe8bc4f8ee51e82f65f1257442aeafaaeb902d
nikita <>
2018-06-05 12:10:52 +0300

    eventEmitter

--- eventEmmiter.js
+++ eventEmmiter.js
@@ -1,19 +1,15 @@
 class User {
   constructor(name) {
-    this.name = name;
+    this._name = name;
     this.openNewNews = this.openNewNews.bind(this);
   }
-  openNewNews(data) {
-    // console.log(this);
-    console.log(this.name, data);
-    // const t = data => {
-    //   console.log(this.name, data);
-    // };
-    // return t;
+  openNewNews(topicName, data) {
+    console.log(this._name, topicName, data);
   }
 }
+
 class News {
-  constructor() {
+  constructor(eventEm) {
     this.newsArray = [
       'Зидан покинул пост главного тренера Реала',
       'Официально: Матч Албания — Украина состоится 3 июня',
@@ -21,67 +17,77 @@ class News {
       'Винни пух',
       'talk is cheap show me the code',
     ];
+    this._topics = ['it', 'sport', 'garden'];
+    this._ev = eventEm;
   }
   feedNews() {
-    const numberNews = Math.floor(Math.random() * this.newsArray.length);
-    return this.newsArray[numberNews];
+    const numberNews = this.randomiser(this.newsArray);
+    const numberTopic = this.randomiser(this._topics);
+    this._ev.emit(this._topics[numberTopic], this.newsArray[numberNews]);
+  }
+  randomiser(maxLength) {
+    const randomNumber = Math.floor(Math.random() * maxLength.length);
+    return randomNumber;
   }
 }
 
 class EventEmitter {
   constructor() {
-    this.events = Object.create(null);
+    this._events = Object.create(null);
   }
   on(name, fn) {
-    // const arrayEvents = Object.keys(this.events);
-    // const index = arrayEvents.includes(name);
-    // if(index){
-    //   this.events[name] =
-    // }
-    if (!this.events[name]) {
-      this.events[name] = [];
-      this.events[name].push(fn);
+    if (!this._events[name]) {
+      this._events[name] = [];
+      this._events[name].push(fn);
     } else {
-      this.events[name].push(fn);
+      this._events[name].push(fn);
     }
   }
   del(name, fn) {
-    if (this.events[name]) {
-      this.events[name] = this.events[name].filter(el => {
+    if (this._events[name]) {
+      this._events[name] = this._events[name].filter(el => {
         if (el !== fn) {
           return true;
         }
         return false;
       });
+
+      if (!this._events[name].length) {
+        delete this._events[name];
+      }
     }
   }
-  once(name, fn) {
-    // add function
-    // how delete function after first using
-  }
+  // once(name, fn) {
+  // -- add function
+  // -- how delete function after first using
+  // }
 
   emit(name, data) {
-    if (this.events[name]) {
-      this.events[name].forEach(func => {
-        func(data);
-        func.call(null, data);
+    if (this._events[name]) {
+      this._events[name].forEach(func => {
+        // func(data);
+        func.call(null, name, data);
       });
     }
   }
 }
-const news = new News();
+
 const user1 = new User('zasranec');
 const user2 = new User('takoe');
 const user3 = new User('goood');
 
 const eventEmitter = new EventEmitter();
-eventEmitter.on(user1.name, user1.openNewNews);
-eventEmitter.on(user2.name, user1.openNewNews);
-function c() {
-  console.log(this.name);
-}
-eventEmitter.on(user1.name, c);
+eventEmitter.on('sport', user1.openNewNews);
+eventEmitter.on('it', user1.openNewNews);
+eventEmitter.on('it', user2.openNewNews);
+eventEmitter.on('it', user3.openNewNews);
+eventEmitter.on('garden', user3.openNewNews);
+eventEmitter.on('garden', user1.openNewNews);
+const news = new News(eventEmitter);
+// const news2 = new News('sport', eventEmitter);
+news.feedNews();
+news.feedNews();
 
-eventEmitter.emit(user1.name, news.feedNews());
-eventEmitter.del(user1.name, c);
-eventEmitter.emit(user1.name, news.feedNews());
+eventEmitter.del('sport', user1.openNewNews);
+eventEmitter.del('garden', user1.openNewNews);
+news.feedNews();
